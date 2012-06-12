c5e8b2af7e68bd6328460ba856f9043652d6dda6
James Stevenson <james.stevenson@threespot.com>
2012-06-12 15:01:54 -0400

    Adds the static URL helper function to provide an easy way to serve uploaded media in a development environment.

--- project_template/project_name/urls.py
+++ project_template/project_name/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls import include, patterns, static, url
 
 from django.contrib import admin
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', '{{ project_name }}.views.home', name='home'),
     # url(r'^{{ project_name }}/', include('{{ project_name }}.foo.urls')),
-
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/passwords/',include('{{ project_name }}.utils.password_urls')
     ),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=True)
